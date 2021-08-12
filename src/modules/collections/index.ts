e79d7788d1f0e179660a0216de49b748fcda389a
Andrés Fernández <andresavena19@gmail.com>
2021-08-12 13:16:03 -0500

    fix: null id on collections

--- src/modules/collections/index.ts
+++ src/modules/collections/index.ts
@@ -1,4 +1,4 @@
-import { Bytes } from "@graphprotocol/graph-ts";
+import { Bytes, log } from "@graphprotocol/graph-ts";
 import { integer } from "@protofire/subgraph-toolkit";
 import { DigitalMediaCollection } from "../../../generated/schema";
 
@@ -13,13 +13,18 @@ export namespace collections {
 			entity.storeContractAddress = storeContract
 			entity.creator = creator
 			entity.metadataPath = metadataPath
+			entity.digitalMediaAmount = integer.ZERO
 		}
 		return entity as DigitalMediaCollection
 	}
 	export function increasedigitalMediaAmount(id: string): DigitalMediaCollection {
+		if (id == null) {
+			log.error("the value for collection id was null :: {}", [id])
+		}
 		let entity = DigitalMediaCollection.load(id)
 		if (entity == null) {
-			return null as DigitalMediaCollection
+			entity = new DigitalMediaCollection(id)
+			entity.digitalMediaAmount = integer.ZERO
 		}
 		entity.digitalMediaAmount = entity.digitalMediaAmount.plus(integer.ONE)
 		return entity as DigitalMediaCollection
